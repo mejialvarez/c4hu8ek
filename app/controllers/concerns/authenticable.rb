128c0778466e892aaa71e6ef3c4d494c5018d892
Hamilton Alvarez Mejia <alvarezhamilton@gmail.com>
2016-04-15 00:32:52 -0500

    Refactor authenticate with token

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -1,10 +1,14 @@
 module Authenticable
   def user_valid?
-    User.where(email: request.headers['X-User-Email'], api_token: request.headers['X-Api-Token']).present?
+    email = request.headers['X-User-Email']
+    api_token = request.headers['X-Api-Token']
+
+    @current_user ||= User.find_by(email: email)
+
+    (@current_user && @current_user.api_token == api_token) ? true : false
   end
 
   def authenticate_with_token!
-    render json: { errors: "Not authenticated" },
-                   status: :unauthorized unless user_valid?
+    head 401 unless user_valid?
   end
 end
